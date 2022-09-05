1c6fdc96e1cef78050f113ab9efaaa81159c29b9
thaylan-018 <thaylan.santos01@etec.sp.gov.br>
2022-09-05 17:11:15 +0000

    main

---
+++ app/src/main/java/exspring/models/Genero.java
@@ -0,0 +1,33 @@
+package exspring.models;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+}
