a681fffc02f1e66aa3c3e567b04d955ef2c12d64
thaylan-018 <thaylan.santos01@etec.sp.gov.br>
2022-09-19 17:56:23 +0000

    exspring

--- app/src/main/java/exspring/controllers/GeneroController.java
+++ app/src/main/java/exspring/controllers/GeneroController.java
@@ -1,13 +1,20 @@
 package exspring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import exspring.repositories.GeneroRepository;
 
 @Controller 
 @RequestMapping("/generos")
 public class GeneroController {
+    private GeneroRepository generosRepo;
+    @Autowired
     @RequestMapping("list")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("generos", this.generosRepo.findAll());
         return "list";
     }
 }
